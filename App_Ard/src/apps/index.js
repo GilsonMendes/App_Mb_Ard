12755acf1136eadc12d12e055b36d1b603fc5452
GilsonMendes <38471977+GilsonMendes@users.noreply.github.com>
2023-02-07 21:45:17 -0300

    Add button Switch

--- App_Ard/src/apps/index.js
+++ App_Ard/src/apps/index.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { Text, View } from 'react-native';
+import React, { useState } from "react";
+import { Text, View, Switch } from 'react-native';
 import Styles from './styles'
 
 export default props => {
+    const [isEnabled, setIsEnabled] = useState(false);
+    
+    const toggleSwitch = () => setIsEnabled(previousState => !previousState);
 
     return (
         <View style={Styles.container}>
-            <Text>Hello</Text>
+            <Switch
+                trackColor={{ false: '#932', true: '#093' }}
+                thumbColor={isEnabled ? '#DAF7A6' : '#f4f3f4'}
+                ios_backgroundColor="#3e3e3e"
+                onValueChange={toggleSwitch}
+                value={isEnabled}
+            />
         </View>
     )
 
